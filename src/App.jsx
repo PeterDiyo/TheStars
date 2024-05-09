a361e0aa92b38c6fa73663e612061c98e4231660
unknown <PeterDiyo>
2024-05-09 21:22:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { About } from "./components/About";
+import About from "./components/About";
 import Featured from "./components/Featured";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
+import about from "../assets/aboutt.png";
 
-export const About = () => {
+const About = () => {
   return (
-    <div id="about">
-      <SectionTitle text="About page under construction" />
-    </div>
+    <section className="py-10 align-element" id="about">
+      <div className=" grid md:grid-cols-2 items-center gap-8 mb-8">
+        <img src={about} className="w-full h-64 " />
+        <article>
+          <SectionTitle text="About" />
+          <p className="mt-8 ">The About Page is under construction</p>
+        </article>
+      </div>
+    </section>
   );
 };
+export default About;
